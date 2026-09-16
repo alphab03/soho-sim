87274ae61585315fcb99e4c50e2d8fb85ac2b78e
Alpha Barry <201652509+alphab03@users.noreply.github.com>
2026-09-16 11:58:03 -0400

    Agent-world M3: update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'valley-sim-v10';
+const CACHE = 'valley-sim-v11';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
