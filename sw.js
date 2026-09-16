f0fa2a054332bd34cc1618c83e5cd5f566caf0ca
Alpha Barry <201652509+alphab03@users.noreply.github.com>
2026-09-16 15:21:59 -0400

    Civilization v14: update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'valley-sim-v13';
+const CACHE = 'valley-sim-v14';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
