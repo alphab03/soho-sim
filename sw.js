cbfc013bba03988cc0f63a0c54cdbbf09ad9dfef
Alpha Barry <201652509+alphab03@users.noreply.github.com>
2026-09-16 03:47:05 -0400

    Valley milestone 7: update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'valley-sim-v7';
+const CACHE = 'valley-sim-v8';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
