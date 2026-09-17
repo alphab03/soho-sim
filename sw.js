1936e90af5d937cd99ec0206e1a88da48886b6f4
Alpha Barry <201652509+alphab03@users.noreply.github.com>
2026-09-16 22:49:07 -0400

    Deploy home v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'valley-sim-v18';
+const CACHE = 'valley-sim-v19';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', (e) => {
